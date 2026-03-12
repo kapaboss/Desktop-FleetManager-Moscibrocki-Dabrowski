1a34ed7921f82efa831dfeaa9650fe1d69f9a984
kapa <kapa@goat.pl>
2026-03-12 09:32:25 +0100

    Funkcje dodawania i zapisywani do JSON

--- FleetManager/ViewModels/MainWindowViewModel.cs
+++ FleetManager/ViewModels/MainWindowViewModel.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Linq;
+using System.Reactive;
 using System.Reactive.Linq;
 using System.Text.Json;
 using FleetManager.Models;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
 
 namespace FleetManager.ViewModels;
 
@@ -12,11 +16,66 @@ public class MainWindowViewModel : ViewModelBase
 {
     private const string FilePath = "Data/vehicles.json";
 
+    private static readonly JsonSerializerOptions JsonOptions = new() { WriteIndented = true };
+
     public ObservableCollection<Vehicle> Vehicles { get; } = [];
+    
+    [Reactive] public string NewName { get; set; } = string.Empty;
+    [Reactive] public string NewLicenseNumber { get; set; } = string.Empty;
+    [Reactive] public int NewFuelPercentage { get; set; } = 0;
+    [Reactive] public string NewStatus { get; set; } =  string.Empty;
+    
+    public ReactiveCommand<Unit, Unit> AddCommand { get; }
+    public ReactiveCommand<Unit, Unit> SaveCommand { get; }
+    
 
     public MainWindowViewModel()
     {
         LoadVehicles();
+
+        AddCommand = ReactiveCommand.Create(AddVehicles);
+        SaveCommand = ReactiveCommand.Create(SaveToJson);
+    }
+
+    private void AddVehicles()
+    {
+        if (new List<string> { NewName, NewLicenseNumber, NewStatus }.Any(string.IsNullOrWhiteSpace) || NewFuelPercentage <= 0)
+        {
+            Console.WriteLine("All fields have to be filled correctly.");
+            return;
+        }
+        
+        Vehicles.Add(new Vehicle
+        {
+            Name = NewName,
+            LicenceNumber = NewLicenseNumber,
+            FuelPercentage = NewFuelPercentage,
+            Status = NewStatus
+        });
+        NewName = NewLicenseNumber = NewStatus = string.Empty;
+        NewFuelPercentage = 0;
+    }
+
+    private void SaveToJson()
+    {
+        try
+        {
+            File.WriteAllText(FilePath, JsonSerializer.Serialize(Vehicles, JsonOptions));
+            Console.WriteLine("JSON file saved successfully.");
+
+        }
+        catch (Exception exception)
+            when (exception is
+            IOException or
+            UnauthorizedAccessException or
+            JsonException)
+        {
+            Console.WriteLine($"Save File Error {exception.Message}");
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine($"Unexpected error (report this!): {exception.Message}");
+        }
     }
 
     private void LoadVehicles()
