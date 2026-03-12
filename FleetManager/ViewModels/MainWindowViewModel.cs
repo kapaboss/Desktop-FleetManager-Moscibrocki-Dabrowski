44ad70ae181948ee717e3798b84069ac28020bb1
kapa <kapa@goat.pl>
2026-03-12 09:15:19 +0100

    Definicja modelu danych i przykładowych danych

--- FleetManager/ViewModels/MainWindowViewModel.cs
+++ FleetManager/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,43 @@
-﻿namespace FleetManager.ViewModels;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Reactive.Linq;
+using System.Text.Json;
+using FleetManager.Models;
+
+namespace FleetManager.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    private const string FilePath = "Data/vehicles.json";
+
+    public ObservableCollection<Vehicle> Vehicles { get; } = [];
+
+    public MainWindowViewModel()
+    {
+        LoadVehicles();
+    }
+
+    private void LoadVehicles()
+    {
+        if(!File.Exists(FilePath)) return;
+        try
+        {
+            var jsonData = File.ReadAllText(FilePath);
+            var list = JsonSerializer.Deserialize<List<Vehicle>>(jsonData);
+            Vehicles.Clear();
+            if(list == null) return;
+            foreach (var vehicle in list)
+            {
+                Vehicles.Add(vehicle);
+            }
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
 }
