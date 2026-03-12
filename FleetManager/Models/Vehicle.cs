2765b2d98ef81d58cb037ae382d04618fc129215
kapa <kapa@goat.pl>
2026-03-12 09:09:21 +0100

    Definicja wyglądu aplikacji

--- FleetManager/Models/Vehicle.cs
+++ FleetManager/Models/Vehicle.cs
@@ -2,5 +2,9 @@ namespace FleetManager.Models;
 
 public class Vehicle
 {
+    public string Name { get; set; }
+    public string LicenceNumber {get; set;}
+    public int FuelPercentage {get; set;}
+    public string Status {get; set;}
     
 }
--- FleetManager/Views/MainWindow.axaml.cs
+++ FleetManager/Views/MainWindow.axaml.cs
@@ -1,8 +1,10 @@
 using Avalonia.Controls;
+using FleetManager.ViewModels;
+using ReactiveUI.Avalonia;
 
 namespace FleetManager.Views;
 
-public partial class MainWindow : Window
+public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
 {
     public MainWindow()
     {
